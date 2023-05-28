66fb5b2612f8b4f461e03d6ff07eb79c0a947cdb
OkaOka <2900034525@qq.com>
2023-05-28 11:22:34 +0800

    框架搭建

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
-# 这是一个示例 Python 脚本。
+def split_datasets(file_name, validation_ratio):
+    """
+        将数据集划分为训练集和验证集，并将训练集储存在train_data.txt中，
+        验证集储存在validation_data.txt中。
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+        args:
+            file_name: train.txt的文件路径
+            validation_ratio (float): 验证集所占的比例，取值范围为 (0, 1)。
+
+        return:
+        无
+    """
+    # TODO()
+    return
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi 1, {name}')  # 按 Ctrl+F8 切换断点。
+execute_split = True
 
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def preprocess_user(file_name):
+    """
+        预处理user，统计user信息，并存储为id : avg_score的形式，输出文件名字为user_info.txt
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+        args:
+            file_name: train.txt或者train_data.txt的文件路径
+        return:
+            无
+    """
+    # TODO()
+    return
+
+
+execute_preprocess_user = True
+
+
+def preprocess_item(file_name):
+    """
+        预处理item，统计item信息，并存储为id : avg_score的形式，输出文件名字为item_info.txt
+
+        args:
+            file_name: train.txt或者train_data.txt的文件路径（如果用item_attr的话，改一下注释）
+        return:
+            无
+    """
+    # TODO()
+    return
+
+
+execute_preprocess_item = True
+
+mode = "test"  # 该mode下，需要将train划分，将train_data.txt作为训练集，validation_data.txt作为验证集
+# mode = "report" # 该mode下，不需要将train划分，将train.txt作为训练集，test.txt作为测试集
+
+if __name__ == "__main__":
+    if mode == "test":
+        # 数据预处理
+        if execute_split:  # 如果不需要重新划分数据集，该变量请设置为false
+            split_datasets("train.txt", 0.2)
+            preprocess_user("train_data.txt")
+            preprocess_item("train_data.txt")
+
+        # TODO()
+    elif mode == "report":
+        # 数据预处理
+        if execute_preprocess_user:  # 如果已经生成了user_info.txt，就不用重复执行这个函数了，该变量请设置为false
+            preprocess_user("train_data.txt")
+        if execute_preprocess_item:  # 如果已经生成了item_info.txt，就不用重复执行这个函数了，该变量请设置为false
+            preprocess_item("train_data.txt")
+
+        # TODO()
